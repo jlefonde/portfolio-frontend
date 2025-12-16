2c6a46106d9c38fe163a24c4e81bace5e4e13ac3
jlefonde <joris.lefondeur@protonmail.com>
2025-12-16 12:25:42 +0100

    Added filters in Skills

--- src/components/Tag.vue
+++ src/components/Tag.vue
@@ -2,18 +2,21 @@
 import type { Tag } from "../types";
 import { Icon } from "@iconify/vue";
 
-withDefaults(defineProps<Tag>(), {
+withDefaults(defineProps<Tag & {
+  as?: "div" | "button"
+}>(), {
+  as: "div",
   textColor: "base-50",
   bgColor: "base-200",
 });
 </script>
 
 <template>
-  <div
+  <component :is="as"
     class="flex cursor-pointer rounded-full px-3 py-1 text-xs font-bold hover:scale-105"
     :class="`bg-${bgColor} text-${textColor}`"
   >
     <Icon v-if="icon" :icon="icon" class="mr-2 size-4" />
     <span class="m-auto text-center">{{ name }}</span>
-  </div>
+  </component>
 </template>
--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -3,15 +3,16 @@ import Gauge from "../components/Gauge.vue";
 import Title from "../components/Title.vue";
 import VerticalBarChart from "../components/VerticalBarChart.vue";
 import SkillItem from "../components/SkillItem.vue";
+import Tag from "../components/Tag.vue";
 import { routes } from "../router";
 import { skills } from "../data/skills";
 import { useRoute } from "vue-router";
 import { getColorHex } from "../composables/color";
 import { Chart as ChartJS, RadialLinearScale, PointElement, LineElement, Filler, Tooltip } from "chart.js";
 import { Radar } from "vue-chartjs";
-import type { Bar } from "../types";
 import { Icon } from "@iconify/vue";
-import { ref } from "vue";
+import { computed, ref } from "vue";
+import type { Bar } from "../types";
 
 const route = useRoute();
 const navRoute = routes.find((r) => r.path == route.path);
@@ -19,12 +20,16 @@ const navRoute = routes.find((r) => r.path == route.path);
 let skillBarChart: Bar[] = [];
 const categories = new Map<string, { sum: number; count: number }>();
 
-skills.forEach((skill) => {
-  skill.stack.categories
-    .filter((item, pos) => {
-      return skill.stack.categories.indexOf(item) == pos;
-    })
-    .forEach((category) => {
+const skillCategories = Array.from(
+  new Set(skills.flatMap((skill) => skill.stack.categories.map((cat) => cat.name))),
+).map((name) => {
+  return skills.flatMap((skill) => skill.stack.categories).find((cat) => cat.name === name)!;
+});
+
+skills
+  .filter((s) => s.featured)
+  .forEach((skill) => {
+    skill.stack.categories.forEach((category) => {
       const mapCategory = categories.get(category.name);
       if (mapCategory == null) {
         categories.set(category.name, { sum: skill.percentage, count: 1 });
@@ -34,8 +39,8 @@ skills.forEach((skill) => {
       }
     });
 
-  skillBarChart.push({ label: skill.stack.name, percentage: skill.percentage });
-});
+    skillBarChart.push({ label: skill.stack.name, percentage: skill.percentage });
+  });
 
 let categoriesMean: number[] = [];
 categories.forEach((value, _) => {
@@ -85,22 +90,42 @@ const radarChart = {
     },
   },
 };
+
 ChartJS.register(RadialLinearScale, PointElement, LineElement, Filler, Tooltip);
 
-const overviewSelected = ref(true);
+function setActiveCategoryFilter(index: number) {
+  activeCategoryFilter.value = index;
+}
+
+const overviewSelected = ref(false);
 const showProficiency = ref(false);
+const activeCategoryFilter = ref(-1);
+const skillSearch = ref("");
+
+const filteredSkills = computed(() => {
+  const selectedCategory = skillCategories[activeCategoryFilter.value];
+  const filteredSkills = skills.filter((s) => s.stack.name.toLowerCase().startsWith(skillSearch.value.toLowerCase()))
+
+  return filteredSkills.filter((skill) => {
+    if (activeCategoryFilter.value == -1) {
+      return true;
+    }
+
+    if (!selectedCategory) {
+      return false;
+    }
+
+    return skill.stack.categories.some((cat) => cat.name === selectedCategory.name);
+  });
+});
 </script>
 
 <template>
   <div class="mb-5 flex flex-col items-center">
     <div class="flex gap-2 rounded-xl p-2">
       <button
-        class="flex cursor-pointer bg-base-300 items-center gap-2 rounded-sm px-5 py-3 hover:opacity-75"
-        :class="
-          overviewSelected
-            ? `text-${navRoute?.primaryColor}`
-            : `text-base-50`
-        "
+        class="bg-base-300 flex cursor-pointer items-center gap-2 rounded-sm px-5 py-3 hover:opacity-75"
+        :class="overviewSelected ? `text-${navRoute?.primaryColor}` : `text-base-50`"
         @click="overviewSelected = true"
         title="Show overview charts"
       >
@@ -108,12 +133,8 @@ const showProficiency = ref(false);
         Overview
       </button>
       <button
-        class="flex cursor-pointer items-center bg-base-300 gap-2 rounded-sm px-5 py-3 hover:opacity-75"
-        :class="
-          !overviewSelected
-            ? `text-${navRoute?.primaryColor}`
-            : `text-base-50`
-        "
+        class="bg-base-300 flex cursor-pointer items-center gap-2 rounded-sm px-5 py-3 hover:opacity-75"
+        :class="!overviewSelected ? `text-${navRoute?.primaryColor}` : `text-base-50`"
         @click="overviewSelected = false"
         title="Show detailed skills list"
       >
@@ -143,7 +164,7 @@ const showProficiency = ref(false);
   </div>
   <div v-else class="mb-5 flex flex-col items-center gap-5">
     <div class="flex w-1/2 gap-3">
-      <div class="relative h-14 rounded-xl flex-1">
+      <div class="relative h-14 flex-1 rounded-xl">
         <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
           <Icon icon="lucide:search" class="text-base-50 size-5" />
         </div>
@@ -152,19 +173,35 @@ const showProficiency = ref(false);
           class="text-base-50 bg-base-300 block h-full w-full rounded-xl p-3 pl-10 text-sm"
           type="text"
           maxlength="64"
+          v-model="skillSearch"
         />
       </div>
       <button
-        class="flex items-center gap-2 bg-base-300 rounded-xl p-3 hover:opacity-75 cursor-pointer size-14"
+        class="bg-base-300 flex size-14 cursor-pointer items-center gap-2 rounded-xl p-3 hover:opacity-75"
         :class="showProficiency ? `text-${navRoute?.primaryColor}` : 'text-base-50'"
         @click="showProficiency = !showProficiency"
         title="Toggle proficiency display"
       >
-        <Icon icon="lucide:gauge" class="size-5 m-auto" />
+        <Icon icon="lucide:gauge" class="m-auto size-5" />
       </button>
     </div>
+    <div class="flex w-1/2 flex-wrap gap-x-2 gap-y-3">
+      <Tag
+        name="All"
+        as="button"
+        :text-color="activeCategoryFilter == -1 ? `${navRoute?.primaryColor}` : `base-50`"
+        @click="setActiveCategoryFilter(-1)"
+      />
+      <Tag
+        v-for="(category, index) in skillCategories"
+        v-bind="category"
+        as="button"
+        :text-color="activeCategoryFilter == index ? `${navRoute?.primaryColor}` : `base-50`"
+        @click="setActiveCategoryFilter(index)"
+      />
+    </div>
     <div class="grid w-full grid-cols-4 gap-3">
-      <SkillItem v-for="skill in skills" v-bind="skill" :show-proficiency="showProficiency" />
+      <SkillItem v-for="skill in filteredSkills" v-bind="skill" :show-proficiency="showProficiency" />
     </div>
   </div>
 </template>
