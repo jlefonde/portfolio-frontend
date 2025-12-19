3227b1c25cff6f0bc776616e43bb8d9020c2f651
jlefonde <joris.lefondeur@protonmail.com>
2025-12-19 15:50:23 +0100

    Added mobile support for Certification

--- src/views/Certifications.vue
+++ src/views/Certifications.vue
@@ -5,10 +5,10 @@ import Stat from '../components/Stat.vue'
 </script>
 
 <template>
-  <div class="mb-5 grid grid-cols-5 gap-5">
+  <div class="mb-5 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-5">
     <Stat class="min-h-56" name="Certifications Earned" :value="certifications.length.toString()" color="blue-light" />
   </div>
-  <div class="grid grid-cols-3 gap-5">
+  <div class="grid sm:grid-cols md:grid-cols-2 2xl:grid-cols-3 gap-5">
     <Certification class="min-h-72" v-for="cert in certifications" :key="cert.name" v-bind="cert" />
   </div>
 </template>
